95f6dc2632c41e757e431a6f782f3d756fe96c9c
tnganh0027 <tnganh0027@gmail.com>
2018-03-07 11:43:45 +0700

    MockMvc test

--- src/test/java/com/ngocanh/spring5recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/com/ngocanh/spring5recipeapp/controllers/IndexControllerTest.java
@@ -7,6 +7,9 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -19,6 +22,11 @@ import static org.mockito.Mockito.verify;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.RequestBuilder.*;
+
 
 public class IndexControllerTest {
 
@@ -37,6 +45,15 @@ public class IndexControllerTest {
 
         controller = new IndexController(recipeService);
 
+    }
+
+    @Test
+    public void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 
     @Test
