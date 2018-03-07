bce63f739c701299d77a72d1fcd4e0b1b7097502
tnganh0027 <tnganh0027@gmail.com>
2018-03-07 10:30:28 +0700

    using Mockito create test

---
+++ src/test/java/com/ngocanh/spring5recipeapp/domain/CategoryTest.java
@@ -0,0 +1,36 @@
+package com.ngocanh.spring5recipeapp.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp(){
+
+
+
+        category = new Category();
+    }
+
+    @Test
+    public void getId() {
+
+        Long idValue = 4L;
+        category.setId(idValue);
+
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void getRecipes() {
+    }
+}
