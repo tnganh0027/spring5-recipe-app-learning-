72038a1d21a0b121f2ce87c8c195cc70ed01a995
tnganh0027 <tnganh0027@gmail.com>
2018-03-12 11:45:37 +0700

    fix RecipeServiceImplTest

--- src/test/java/com/ngocanh/spring5recipeapp/services/RecipeServiceImplTest.java
+++ src/test/java/com/ngocanh/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -1,5 +1,7 @@
 package com.ngocanh.spring5recipeapp.services;
 
+import com.ngocanh.spring5recipeapp.converters.RecipeCommandToRecipe;
+import com.ngocanh.spring5recipeapp.converters.RecipeToRecipeCommand;
 import com.ngocanh.spring5recipeapp.domain.Recipe;
 import com.ngocanh.spring5recipeapp.repositories.RecipeRepository;
 import org.junit.Before;
@@ -22,12 +24,17 @@ public class RecipeServiceImplTest {
     @Mock
     RecipeRepository recipeRepository;
 
+    @Mock
+    RecipeToRecipeCommand recipeToRecipeCommand;
+
+    @Mock
+    RecipeCommandToRecipe recipeCommandToRecipe;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
 
-        recipeService = new RecipeServiceImpl(recipeRepository);
+        recipeService = new RecipeServiceImpl(recipeRepository, recipeCommandToRecipe, recipeToRecipeCommand);
     }
 
     @Test
