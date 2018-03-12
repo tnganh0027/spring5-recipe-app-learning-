b45b5a44aa7079e8c81c83db89c8a625ebd702e1
tnganh0027 <tnganh0027@gmail.com>
2018-03-12 11:37:34 +0700

    add unit test

--- src/main/java/com/ngocanh/spring5recipeapp/services/RecipeService.java
+++ src/main/java/com/ngocanh/spring5recipeapp/services/RecipeService.java
@@ -1,5 +1,6 @@
 package com.ngocanh.spring5recipeapp.services;
 
+import com.ngocanh.spring5recipeapp.commands.RecipeCommand;
 import com.ngocanh.spring5recipeapp.domain.Recipe;
 
 import java.util.Set;
@@ -8,4 +9,6 @@ public interface RecipeService {
 
     Set<Recipe> getRecipes();
     Recipe findById(Long l);
+
+    RecipeCommand saveRecipeCommand(RecipeCommand command);
 }
--- src/main/java/com/ngocanh/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/ngocanh/spring5recipeapp/services/RecipeServiceImpl.java
@@ -1,20 +1,29 @@
 package com.ngocanh.spring5recipeapp.services;
 
+import com.ngocanh.spring5recipeapp.commands.RecipeCommand;
+import com.ngocanh.spring5recipeapp.converters.RecipeCommandToRecipe;
+import com.ngocanh.spring5recipeapp.converters.RecipeToRecipeCommand;
 import com.ngocanh.spring5recipeapp.domain.Recipe;
 import com.ngocanh.spring5recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
-
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
+    private final RecipeCommandToRecipe recipeCommandToRecipe;
+    private final RecipeToRecipeCommand recipeToRecipeCommand;
 
-    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+    public RecipeServiceImpl(RecipeRepository recipeRepository, RecipeCommandToRecipe recipeCommandToRecipe, RecipeToRecipeCommand recipeToRecipeCommand){
         this.recipeRepository = recipeRepository;
+        this.recipeCommandToRecipe = recipeCommandToRecipe;
+        this.recipeToRecipeCommand = recipeToRecipeCommand;
     }
 
     @Override
@@ -34,6 +43,19 @@ public class RecipeServiceImpl implements RecipeService {
         if(!recipeOptional.isPresent()) throw new RuntimeException("Recipe not found");
 
         return recipeOptional.get();
+    }
+
+    @Override
+    @Transactional
+    public RecipeCommand saveRecipeCommand(RecipeCommand command) {
+
+        Recipe detacheRecipe = recipeCommandToRecipe.convert(command);
+
+        Recipe savedRecipe = recipeRepository.save(detacheRecipe);
+        log.debug("Saved Recipe: " + savedRecipe.getId());
+
+        return recipeToRecipeCommand.convert(savedRecipe);
+
     }
 
 }
