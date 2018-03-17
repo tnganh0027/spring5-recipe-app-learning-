b91641dc4f30ad58b8eb5d36b06d656a01a5810e
tnganh0027 <tnganh0027@gmail.com>
2018-03-17 11:31:12 +0700

    create recipe from recipeform

--- src/main/java/com/ngocanh/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/com/ngocanh/spring5recipeapp/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package com.ngocanh.spring5recipeapp.controllers;
 
+import com.ngocanh.spring5recipeapp.commands.RecipeCommand;
+import com.ngocanh.spring5recipeapp.domain.Recipe;
 import com.ngocanh.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RecipeController {
@@ -20,5 +23,12 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
     }
 }
