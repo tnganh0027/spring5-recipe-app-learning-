19f5fa393badd1f729ebf44b80b6daecb42c33b5
tnganh0027 <tnganh0027@gmail.com>
2018-04-09 10:29:26 +0700

    view the ingredients

---
+++ src/main/java/com/ngocanh/spring5recipeapp/controllers/IngredientController.java
@@ -0,0 +1,30 @@
+package com.ngocanh.spring5recipeapp.controllers;
+
+import com.ngocanh.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class IngredientController {
+
+    private final RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping
+    @RequestMapping("/recipe/{recipeId}/ingredients")
+    public String listIngredients(@PathVariable String recipeId, Model model){
+        log.debug("Getting ingredients list for recipe id: " + recipeId );
+
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
+
+        return "recipe/ingredient/list";
+    }
+}
